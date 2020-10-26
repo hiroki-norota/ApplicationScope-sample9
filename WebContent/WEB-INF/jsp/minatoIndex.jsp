1c65aedb0039e4cd9cf2b46df6a81db1b2864a90
hiroki-norota <cake.fujiwara@gmail.com>
2020-10-26 21:37:23 +0900

    seconds commit

--- WebContent/WEB-INF/jsp/minatoIndex.jsp
+++ WebContent/WEB-INF/jsp/minatoIndex.jsp
@@ -1,12 +1,24 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="model.SiteEV" %>
+<%
+SiteEV siteEV = (SiteEV) application.getAttribute("siteEV");
+%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>湊くんのページ</title>
 </head>
 <body>
-
+<h1>湊くんのページへようこそ</h1>
+<p>
+<a href="/ApplicationScope-sample9/MinatoIndex?action=like">よいね</a>
+<%= siteEV.getLike() %>人
+<a href="/ApplicationScope-sample9/MinatoIndex?action=dislike">よくないね</a>
+<%= siteEV.getDislike() %>人
+</p>
+<h2>湊くんとは！？</h2>
+<p>・・・</p>
 </body>
 </html>
--- src/servlet/MinatoIndex.java
+++ src/servlet/MinatoIndex.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.SiteEV;
+import model.SiteEVLogic;
 
 /**
  * Servlet implementation class MinatoIndex
@@ -19,7 +25,28 @@ public class MinatoIndex extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		ServletContext application = this.getServletContext();
+		SiteEV siteEV = (SiteEV) application.getAttribute("siteEV");
+
+		if (siteEV == null) {
+			siteEV = new SiteEV();
+		}
+
+		request.setCharacterEncoding("UTF-8");
+		String action = request.getParameter("action");
+
+		SiteEVLogic siteEVLogic = new SiteEVLogic();
+		if (action != null && action.equals("like")) {
+			siteEVLogic.like(siteEV);
+		} else if(action != null && action.equals("dislike")) {
+			siteEVLogic.dislike(siteEV);
+		}
+		application.setAttribute("siteEV", siteEV);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/minatoIndex.jsp");
+		dispatcher.forward(request, response);
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 }
